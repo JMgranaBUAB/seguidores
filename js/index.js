fdc6c99b51785505bc42993545523db958d8f4f5
GranainoBUAB <josemiguel@quesada.eu>
2022-09-28 18:48:10 +0200

    change size font and logo youtube

--- js/index.js
+++ js/index.js
@@ -19,16 +19,16 @@ fetchMoviesJson().then(youtubeStats => {
         <table class="table">
             <thead>
                 <tr>
-                    <th scope="col">Seguidores</th>
-                    <th scope="col">Vistas</th>
-                    <th scope="col">Videos</th>
+                    <th scope="col" class="h2">Seguidores</th>
+                    <th scope="col" class="h2">Vistas</th>
+                    <th scope="col" class="h2">Videos</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td class="text-center">${youtubeSubscriberCount}</td>
-                    <td class="text-center">${youtubeViewCount}</td>
-                    <td class="text-center">${youtubevideoCount}</td>
+                    <td class="text-center h2">${youtubeSubscriberCount}</td>
+                    <td class="text-center h2">${youtubeViewCount}</td>
+                    <td class="text-center h2">${youtubevideoCount}</td>
                 </tr>
             </tbody>
         </table>
